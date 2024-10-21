65f2ff2d341a0a94076e6def74782a3ed0acf33a
Marcantoine241 <marcantoineessone@gmail.com>
2024-10-21 23:18:37 +0100

    Update config.cjs

--- config.cjs
+++ config.cjs
@@ -3,7 +3,7 @@ const fs = require("fs");
 require("dotenv").config();
 
 const config = {
-  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&2W2B6JvJ",
+  SESSION_ID: process.env.SESSION_ID || "Ethix-MD&v6YeTqtP",
   PREFIX: process.env.PREFIX || '.',
   AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
   AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
